ea9af8dcc3f9bdca9b31aca9565c0ee408df03da
Munsterberg <jetrussell93@gmail.com>
2018-07-30 19:51:48 -0400

    create basic CRUD resource service

--- device_registry/__init__.py
+++ device_registry/__init__.py
@@ -1,8 +1,23 @@
 import os
-from flask import Flask
 import markdown
+import shelve
+from flask import Flask, g
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
+api = Api(app)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = shelve.open("devices.db")
+    return db
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def index():
@@ -10,3 +25,52 @@ def index():
         content = markdown_file.read()
 
         return markdown.markdown(content)
+
+class DeviceList(Resource):
+    def get(self):
+        shelf = get_db()
+        keys = list(shelf.keys())
+
+        devices = []
+
+        for key in keys:
+            devices.append(shelf[key])
+
+        return {'message': 'Success', 'data': devices}
+
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('identifier', required=True)
+        parser.add_argument('name', required=True)
+        parser.add_argument('device_type', required=True)
+        parser.add_argument('controller_gateway', required=True)
+
+        args = parser.parse_args()
+
+        shelf = get_db()
+        shelf[args['identifier']] = args
+
+        return {'message': 'Device registered', 'data': args}, 201
+
+
+class Device(Resource):
+    def get(self, identifier):
+        shelf = get_db()
+
+        if not (identifier in shelf):
+            return {'message': 'Device not found', data: {}}, 404
+
+        return {'message': 'Device found', 'data': shelf[identifier]}, 200
+
+    def delete(self, identifier):
+        shelf = get_db()
+
+        if not (identifier in shelf):
+            return {'message': 'Device not found', data: {}}, 404
+
+        del shelf[identifier]
+        return '', 204
+
+api.add_resource(DeviceList, '/devices')
+api.add_resource(Device, '/devices/<string:identifier>')
